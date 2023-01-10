62e2bd7a6b2ed6f4e8d594161dba3900d62e9301
h00kz <jlarrieu@student.42angouleme.fr>
2023-01-10 16:34:34 +0100

    ennnnnnnnnnnnnnv

--- src/builtins/export_utils.c
+++ src/builtins/export_utils.c
@@ -4,8 +4,14 @@ int	ft_check_double_var(t_envp *env, char *arg)
 {
 	while (env)
 	{
-		if (!ft_strncmp(env->variable, arg, (int)ft_strlen(env->variable)))
-			return (0);
+		if (ft_str_index_chr(arg, '=') == -1)
+		{
+			if (!ft_strcmp(env->variable, arg))
+				return (0);
+		}
+		else
+			if (!ft_strncmp(env->variable, arg, (int)ft_strlen(env->variable)))
+				return (0);
 		env = env->next;
 	}
 	return (1);
